b1d46e21ebda5e683f70a2ce9ee6300b8b44dd51
The Nguyen Khac <the.nguyen@codegym.vn>
2020-04-29 15:46:43 +0700

    View Model practice

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ModelBindingTest.Models;
+
+namespace ModelBindingTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        static List<Product> products = new List<Product>();
+
+        [HttpPost]
+        public IActionResult CreateAndUpdate(Product model)
+        {
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                bool isUpdated = false;
+                foreach (Product product in products)
+                {
+                    if (model.Name == product.Name)
+                    {
+                        product.Brand = model.Brand;
+                        product.Description = model.Description;
+                        message = "product " + model.Name + " is updated successfully";
+                        isUpdated = true;
+                        break;
+                    }
+                }
+
+                if (!isUpdated)
+                {
+                    products.Add(model);
+                    message = "product " + model.Name + " created successfully. There're " + products.Count + " products now.";
+                }
+            }
+            else
+            {
+                message = "Failed to create the product. Please try again";
+            }
+            return Content(message);
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,10 @@
+namespace ModelBindingTest.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+    }
+}
